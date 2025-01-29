db9fcc3ea47f9a557f4632c056ab6a8aa5e5edb6
patman <27874300+patmanf@users.noreply.github.com>
2025-01-28 18:28:29 -0600

    hawk config. getParametuah that statuseffect

--- pat/yogurt/stat/yogurtslow.lua
+++ pat/yogurt/stat/yogurtslow.lua
@@ -1,16 +1,12 @@
 function init()
   animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
-  animator.setParticleEmitterActive("drips", true)
-  effect.setParentDirectives("fade=EABAE2=0.8")
-  effect.addStatModifierGroup({
-    {stat = "jumpModifier", amount = -0.2}
-  })
+
+  local get = config.getParameter
+  effect.setParentDirectives(get("directives"))
+  effect.addStatModifierGroup(get("statModifiers"))
+  self.controlModifiers = get("controlModifiers")
 end
 
-function update(dt)
-  mcontroller.controlModifiers({
-		groundMovementModifier = 0.5,
-		speedModifier = 0.65,
-		airJumpModifier = 0.80
-	})
+function update()
+  mcontroller.controlModifiers(self.controlModifiers)
 end
